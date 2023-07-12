981b9fa0e233b3163a2e8cf31706a33f1775758d
Su-daa <subin.kk019@gmail.com>
2023-07-13 00:33:25 +0900

    Style: 평가 상세페이지 디자인 변경

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import LogIn from "./screen/login"
-import SignUp from './screen/signup';
-import HomePage from './screen/HomePage';
-import Mypage from './screen/Mypage';
-
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import LogIn from "./screen/login";
+import SignUp from "./screen/signup";
+import HomePage from "./screen/HomePage";
+import Mypage from "./screen/Mypage";
+import Evaluation from "./screen/Evaluation";
 
 function App() {
   return (
     <>
-
       <Routes>
         <Route path="/" element={<LogIn />} />
         <Route path="/Signup" element={<SignUp />} />
         <Route path="/Home" element={<HomePage />} />
-        <Route path="/MyPage" element={<Mypage/>}/>
+        <Route path="/MyPage" element={<Mypage />} />
+        <Route path="/Evaluation" element={<Evaluation />} />
       </Routes>
-
-      
     </>
   );
 }
--- src/screen/Evaluation.js
+++ src/screen/Evaluation.js
@@ -1,87 +1,44 @@
 import "../styles/styles.css";
-import logoImg from "../src/img/logo.jpg";
-import userImg from "../src/img/user.png";
-import lectureImg from "../src/img/lectureImg.jpg";
+import logoImg from "../img/logo.jpg";
+import userImg from "../img/user.png";
+import lectureImg from "../img/lectureImg.jpg";
+import NavBar from "../components/NavBar";
 
 function Evaluation() {
   return (
     <div>
-      <header>
-        <div className="container">
-          <div className="logo">
-            <img src={logoImg} alt="logo" className="logoImg" />
+      <NavBar />
+      <div className="lectureBox">
+        <div className="lecture-container">
+          <img src={lectureImg} alt="lectureImg" className="lectureImg" />
+          <div className="lecture-info">
+            <h1 className="titleText">강의 사회계층론</h1>
+            <h4 className="detailText">교수명 홍길동</h4>
+            <h4 className="detailText">과목코드 45601-01</h4>
+            <h4 className="detailText">강의평가 10개의 수강평</h4>
           </div>
-          <div className="search">
-            <form action="/search" method="GET">
-              <input
-                type="text"
-                name="query"
-                placeholder="과목명으로 검색"
-                className="searchbar"
-              />
-            </form>
-          </div>
-          <div className="user">
-            <img src={userImg} alt="user" className="userImg" />
-          </div>
-          <div>
-            <button className="mypageButton" />
-            마이페이지
-          </div>
-        </div>
-      </header>
-
-      <div class="lectureBox">
-        <div class="container">
-          <img src={lectureImg} alt="lectureImg" class="lectureImg" />
-          <h1 class="titleText">
-            <button class="emphTitleBox">강의</button> 사회계층론
-          </h1>
-          <h4 class="detailText">교수명 &nbsp;&nbsp;홍길동</h4>
-          <h4 class="detailText">과목코드 &nbsp;&nbsp;45601-01</h4>
-          <h4 class="detailText">강의평가 &nbsp;&nbsp;10개의 수강평</h4>
         </div>
       </div>
 
-      <nav>
-        <ul>
-          <li>
-            <a href="#">개요</a>
-          </li>
-          <li>
-            <a href="#">수강평</a>
-          </li>
-        </ul>
-      </nav>
-
-      <div className="container">
-        <p className="reviewTitle">
-          <h1>강의평</h1>
-          <h4>총 15개</h4>
-        </p>
+      <div className="review-title">
+        <h1>강의평</h1>
+        <h4>총 15개</h4>
       </div>
 
-      <div className="container">
-        <div className="review">
-          <span className="container">
-            <img src={userImg} alt="reviewImg" className="reviewImg" />
-            <span>
-              <p className="speech-bubble">최고의 강의! 정말 유익하다!</p>
-            </span>
-          </span>
-        </div>
-        <button className="likeButton">수정 / 삭제</button>
-        <button className="likeButton">좋아요</button>
-        <h6 className="date">작성일자</h6>
+      <div className="review-container">
+        <img src={userImg} alt="reviewImg" className="reviewImg" />
+        <span className="speech-bubble">contents</span>
+        <button className="btn edit-btn">수정</button>
+        <button className="btn delete-btn">삭제</button>
       </div>
 
-      <div className="container">
+      <div className="review-input-container">
         <input
           className="reviewbox"
           type="text"
-          placeholder="강의평을 입력해주세요."
+          placeholder="한줄평을 입력해주세요."
         />
-        <input className="reviewsubmit" type="submit" />
+        <button className="submit-btn">제출</button>
       </div>
     </div>
   );
