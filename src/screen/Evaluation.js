e5cc1197c3314fa2d56565c2e3254ee9c7788e90
Su-daa <subin.kk019@gmail.com>
2023-07-13 15:50:31 +0900

    Test: 상세페이지 리뷰 화면 테스트

--- src/screen/Evaluation.js
+++ src/screen/Evaluation.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import "../styles/styles.css";
 import logoImg from "../img/logo.jpg";
 import userImg from "../img/user.png";
@@ -5,6 +6,48 @@ import lectureImg from "../img/lectureImg.jpg";
 import NavBar from "../components/NavBar";
 
 function Evaluation() {
+  let reviewList1 = [
+    {
+      reviewId: 0,
+      name: "김수빈",
+      updatedAt: "2023.07.04",
+      star: 5,
+      content: "수업이 너무 지루해",
+    },
+    {
+      reviewId: 1,
+      name: "김수빈",
+      updatedAt: "2023.07.04",
+      star: 5,
+      content: "수업이 너무 지루해1",
+    },
+    {
+      reviewId: 2,
+      name: "김수빈",
+      updatedAt: "2023.07.04",
+      star: 5,
+      content: "수업이 너무 지루해2",
+    },
+    {
+      reviewId: 3,
+      name: "김수빈",
+      updatedAt: "2023.07.04",
+      star: 5,
+      content: "수업이 너무 지루해3",
+    },
+  ];
+
+  /*
+  const [reviewList, setReviewList] = useState([]);
+
+  const getReviewList = async () => {
+    //데이터 받아오기
+  };
+
+  useEffect(() => {
+    getReviewList();
+  }, []);*/
+
   return (
     <div>
       <NavBar />
@@ -12,10 +55,10 @@ function Evaluation() {
         <div className="lecture-container">
           <img src={lectureImg} alt="lectureImg" className="lectureImg" />
           <div className="lecture-info">
-            <h1 className="titleText">강의 사회계층론</h1>
-            <h4 className="detailText">교수명 홍길동</h4>
-            <h4 className="detailText">과목코드 45601-01</h4>
-            <h4 className="detailText">강의평가 10개의 수강평</h4>
+            <h1 className="titleText">강의 </h1>
+            <h4 className="detailText">교수명 </h4>
+            <h4 className="detailText">과목코드</h4>
+            <h4 className="detailText">강의평가</h4>
           </div>
         </div>
       </div>
@@ -25,12 +68,16 @@ function Evaluation() {
         <h4>총 15개</h4>
       </div>
 
-      <div className="review-container">
-        <img src={userImg} alt="reviewImg" className="reviewImg" />
-        <span className="speech-bubble">contents</span>
-        <button className="btn edit-btn">수정</button>
-        <button className="btn delete-btn">삭제</button>
-      </div>
+      {reviewList1.map((review) => (
+        <>
+          <div className="review-container">
+            <img src={userImg} alt="reviewImg" className="reviewImg" />
+            <span className="speech-bubble">{review.content}</span>
+            <button className="btn edit-btn">수정</button>
+            <button className="btn delete-btn">삭제</button>
+          </div>
+        </>
+      ))}
 
       <div className="review-input-container">
         <input
